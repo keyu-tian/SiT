c9430cceedb61da9ca245956c0c8ab94af409889
tky_hm_xps13h <k_code@163.com>
2021-07-13 11:46:39 +0800

    [dbg] forgot to loss.backward()

--- utils.py
+++ utils.py
@@ -77,10 +77,10 @@ class AmpScaler:
         loss.backward()
         if clip_grad is not None:
             assert parameters is not None
-            parameters = list(parameters)
-            print(f'============== type(parameters): {type(parameters)}')
-            print(f'============== len (parameters): {len(parameters)}')
-            print(f'============== len (gard not N): {len([p for p in parameters if p.grad is not None])}')
+            # parameters = list(parameters)
+            # print(f'============== type(parameters): {type(parameters)}')
+            # print(f'============== len (parameters): {len(parameters)}')
+            # print(f'============== len (gard not N): {len([p for p in parameters if p.grad is not None])}')
             norm = float(torch.nn.utils.clip_grad_norm_(parameters, clip_grad))
         else:
             norm = None
